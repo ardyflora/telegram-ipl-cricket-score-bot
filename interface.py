cd5702fd82f424b6b1e35b3d21af847d01f3bac9
ardyflora <eripflo@gmail.com>
2017-05-06 22:46:12 -0400

    fixing appending problem of prettytable

--- interface.py
+++ interface.py
@@ -1,41 +1,41 @@
-from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import sqlite3
 import prettytable
 
 updater = Updater(token='345858240:AAGwmVex4IOZntM48kWWVQH-plFbR3RiQxU')
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 dispatcher = updater.dispatcher
 
-# Add in prettytable
-table = prettytable.PrettyTable(['Teams', 'MAT', 'WON', 'LOST', 'TIED', 'N/R', 'PTS', 'NET RR', 'FOR', 'AGAINST'])
 
 def start(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="I'm a bot, please talk to me!")
+
 
 def echo(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
 
 def caps(bot, update, args):
-	text_caps = ' '.join(args).upper()
-	bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
+    text_caps = ' '.join(args).upper()
+    bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
 
-def readIplPointsFromDatabase():
-	conn = sqlite3.connect('iplPoints.db')
-	c = conn.cursor()
-	ipPoints = c.execute('SELECT * from iplPoints')
-	for el in ipPoints:
-		table.add_row(el)
 
 def iplpoints(bot, update):
-	readIplPointsFromDatabase()
-	print "table now: ",table
-	newList = str(table)
-	print "New list: ",newList
-	bot.sendMessage(chat_id=update.message.chat_id, text=newList)
-
+    table = []
+    conn = sqlite3.connect('iplPoints.db')
+    c = conn.cursor()
+    # Add in prettytable
+    table = prettytable.PrettyTable(
+        ['Teams', 'MAT', 'WON', 'LOST', 'TIED', 'N/R', 'PTS', 'NET RR', 'FOR', 'AGAINST'])
+    ipPoints = c.execute('SELECT * from iplPoints')
+    for el in ipPoints:
+        table.add_row(el)
+    bot.sendMessage(chat_id=update.message.chat_id, text=str(table))
 
 
 start_handler = CommandHandler('start', start)
--- pointTable.py
+++ pointTable.py
@@ -5,10 +5,6 @@ import sqlite3
 conn = sqlite3.connect('iplPoints.db')
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE iplPoints
-#              (Teams text, MAT integer, WON integer, Lost integer, TIED integer, NR integer , PTS integer, NETRR text, FOR text, AGAINST text)''')
-
-
 pointTableUrl = 'http://www.espncricinfo.com/indian-premier-league-2017/engine/series/1078425.html?view=pointstable'
 
 
@@ -16,23 +12,16 @@ tree = lxml.html.parse(pointTableUrl)
 tabInfo = tree.xpath(
     "//div[@class='large-20 medium-20 hide-for-small'][1]/table/tbody/tr")
 
-# Add in prettytable
-#table = prettytable.PrettyTable(
-#    ['Teams', 'MAT', 'WON', 'LOST', 'TIED', 'N/R', 'PTS', 'NET RR', 'FOR', 'AGAINST'])
-
 for trElem in tabInfo[1:]:
     row = []
     for td in trElem:
         row.append(td.text.rstrip())
 
-    # Add rows in prettytable
-    #table.add_row(row)
     c.execute("insert into iplPoints (Teams, MAT, WON,LOST, TIED, NR, PTS, NETRR, FOR, AGAINST) values (?,?,?,?,?,?,?,?,?,?)", [
               row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]])
 
 
-
-#print "IPL Point Table: \n", table
+# print "IPL Point Table: \n", table
 
 conn.commit()
 conn.close()
